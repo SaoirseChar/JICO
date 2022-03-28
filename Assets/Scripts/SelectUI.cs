64dd54074796564b7ae0606684d6d9ea721feee4
Lala-banners <lala.l.banno@hotmail.com>
2022-03-28 11:52:23 +1100

    UI select extra works

--- Assets/Scripts/SelectUI.cs
+++ Assets/Scripts/SelectUI.cs
@@ -6,18 +6,23 @@ public class SelectUI : MonoBehaviour
 {
     public TMP_Text text;
 
+    public GameObject uiObject;
+
     private void Start()
     {
         text.text = "";
+        uiObject.SetActive(false);
     }
 
     private void OnMouseEnter ()
     {
         text.text = name;
+        uiObject.SetActive(true);
     }
  
     private void OnMouseExit ()
     {
         text.text = "";
+        uiObject.SetActive(false);
     }
 }
